b731276aca1b7c3accd92bfa89ed45ff20e80752
IZUMFIN\s.sidorov <s.sidorov@factoringplus.ru>
2021-11-24 10:15:14 +0400

    first create record test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,18 +1,111 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Text;
+unit_test_c_sharpusing System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
-namespace unit_test_c_sharp
+namespace First_Tests
 {
-    public class Tests
+    [TestFixture]
+    public class UntitledTestCase
     {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
         [SetUp]
-        public void Setup()
+        public void SetupTest()
         {
+            driver = new FirefoxDriver();
+            baseURL = "https://yandex.ru/search/?clid=1923018&text=github&lr=194&redircnt=1637732111.1";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
         }
 
         [Test]
-        public void Test1()
+        public void TheUntitledTestCaseTest()
         {
-            Assert.Pass();
+            driver.Navigate().GoToUrl("https://yandex.ru/search/?clid=1923018&text=github&lr=194&redircnt=1637732111.1");
+            driver.FindElement(By.XPath("//ul[@id='search-result']/li[2]/div/h2/a/div[2]/span/b[2]")).Click();
+            driver.Navigate().GoToUrl("https://github.com/login");
+            driver.FindElement(By.Name("commit")).Click();
+            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='/'])[3]/preceding::*[name()='svg'][1]")).Click();
+            driver.FindElement(By.Id("repository_name")).Click();
+            driver.FindElement(By.Id("repository_name")).Clear();
+            driver.FindElement(By.Id("repository_name")).SendKeys("tests_csharp");
+            driver.FindElement(By.XPath("//form[@id='new_repository']/div[4]/button")).Click();
+            driver.FindElement(By.XPath("//a[@id='code-tab']/span")).Click();
+            driver.FindElement(By.XPath("//summary/img")).Click();
+            driver.FindElement(By.XPath("(.//*[normalize-space(text()) and normalize-space(.)='Settings'])[2]/following::button[1]")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
         }
     }
 }
