fb93ca88c6cc4942167586fec3473aee8f07cf8a
IZUMFIN\s.sidorov <s.sidorov@factoringplus.ru>
2021-12-05 17:36:32 +0400

    классы и методы

---
+++ class/Figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_test_c_sharp
+{
+    class Figure
+    {
+        private bool colered = false;
+
+        public bool Colored
+        {
+            get
+            {
+                return colered;
+            }
+            set
+            {
+                colered = value;
+            }
+        }
+    }
+}
+
